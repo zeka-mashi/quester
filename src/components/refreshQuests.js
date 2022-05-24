f48c9203081c0ffba1f6103ed82a1e108f835171
David L <lin.david32@gmail.com>
2022-05-23 23:08:07 -0700

    implement edit quest logic -- title and desc

--- src/components/refreshQuests.js
+++ src/components/refreshQuests.js
@@ -29,6 +29,79 @@ export default function refreshQuests() {
         const edit = document.createElement("div");
         edit.innerHTML = questIcons.edit;
         edit.classList.add("quest-action", "edit-icon");
+
+        edit.addEventListener("click", function (e) {
+            const elm =
+                e.currentTarget.parentElement.parentElement.parentElement;
+            let board =
+                JSON.parse(
+                    localStorage.getItem(elm.getAttribute("data-board"))
+                ) || [];
+            let questName = elm.getAttribute("data-quest");
+            let search = board.find(
+                (quest) => quest["quest-name"] === questName
+            );
+            let editIcon = elm.querySelector(".edit-icon");
+            console.log(elm);
+            if (e.currentTarget.classList.contains("check-icon")) {
+                let titleInput = elm.querySelector(".title-edit");
+                let descInput = elm.querySelector(".desc-edit");
+
+                if (
+                    titleInput.value.length > 2 &&
+                    titleInput.value.length < 51 &&
+                    descInput.value.length > 0 &&
+                    descInput.value.length < 501
+                ) {
+                    if (
+                        confirm("Are you sure you want to save your changes?")
+                    ) {
+                        editIcon.innerHTML = questIcons.edit;
+                        e.currentTarget.classList.remove("check-icon");
+
+                        const newTitle = document.createElement("h3");
+                        newTitle.textContent = titleInput.value;
+                        const newDesc = document.createElement("p");
+                        newDesc.classList.add("quest-desc");
+                        newDesc.textContent = descInput.value;
+                        titleInput.parentNode.replaceChild(
+                            newTitle,
+                            titleInput
+                        );
+                        descInput.parentNode.replaceChild(newDesc, descInput);
+                        elm.setAttribute("data-quest", titleInput.value);
+
+                        /* const idx = board.indexOf(search);
+                    board.splice(idx, 1);
+                    localStorage.setItem(
+                        elm.getAttribute("data-board"),
+                        JSON.stringify(board)
+                    ); */
+                    }
+                } else {
+                    alert(
+                        "Please make sure your quest entry is valid!\n\n- Title has 3 to 50 characters\n- Description has 1 to 500 characters"
+                    );
+                }
+            } else {
+                editIcon.innerHTML = questIcons.check;
+                e.currentTarget.classList.add("check-icon");
+                let title = elm.querySelector("h3");
+                let desc = elm.querySelector("p.quest-desc");
+                const titleInput = document.createElement("input");
+                titleInput.setAttribute("type", "text");
+                titleInput.setAttribute("value", title.textContent);
+                titleInput.classList.add("title-edit");
+                const descInput = document.createElement("textarea");
+                descInput.classList.add("desc-edit");
+                descInput.setAttribute("rows", "5");
+                descInput.setAttribute("cols", "40");
+                descInput.value = desc.textContent;
+                title.parentNode.replaceChild(titleInput, title);
+                desc.parentNode.replaceChild(descInput, desc);
+            }
+        });
+
         const trash = document.createElement("div");
         trash.innerHTML = questIcons.trash;
         trash.classList.add("quest-action", "trash-icon");
@@ -53,12 +126,8 @@ export default function refreshQuests() {
                         JSON.stringify(board)
                     );
                     elm.remove();
-                    console.log("Deleted.");
-                } else {
-                    console.log("Not deleted.");
                 }
             }
-            //console.log(elm);
         });
 
         const dropdown = document.createElement("div");
@@ -73,15 +142,19 @@ export default function refreshQuests() {
         questDesc.classList.add("quest-desc");
         questDesc.textContent = thisBoard[i]["quest-desc"];
 
-        dropdown.addEventListener("click", function () {
-            questDesc.classList.toggle("shown");
-            if (questDesc.classList.contains("shown")) {
-                questDesc.style.maxHeight = questDesc.scrollHeight + "px";
-            } else {
-                questDesc.style.maxHeight = "0px";
+        dropdown.addEventListener("click", function (e) {
+            let elm = e.currentTarget.parentElement.parentElement.parentElement;
+            let questDesc = elm.querySelector(".quest-desc");
+            if (questDesc) {
+                questDesc.classList.toggle("shown");
+                if (questDesc.classList.contains("shown")) {
+                    questDesc.style.maxHeight = questDesc.scrollHeight + "px";
+                } else {
+                    questDesc.style.maxHeight = "0px";
+                }
+                dropdown.classList.toggle("drop-active");
+                dropdown.classList.toggle("drop-inactive");
             }
-            dropdown.classList.toggle("drop-active");
-            dropdown.classList.toggle("drop-inactive");
         });
 
         const priorityAndBoard = document.createElement("div");
